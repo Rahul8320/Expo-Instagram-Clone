f0cec78186f67d6e04a1ef57c7b46aee26b07131
Rahul Dey <rahuldey832000@gmail.com>
2024-07-09 14:59:06 +0530

    Add native wind and setup taliwind css

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,8 +2,10 @@ import { View, Text } from "react-native";
 
 export default function FeedScreen() {
   return (
-    <View>
-      <Text>FeedScreen</Text>
+    <View className="bg-slate-300">
+      <Text className="text-5xl font-bold text-indigo-600 text-center p-3 m-10">
+        Feed Screen
+      </Text>
     </View>
   );
 }
